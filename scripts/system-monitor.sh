f5d36d4510ea99651c831b1f7c80c5ac282b6bc9
Kelechi <Kelechidikexx@gmail.com>
2026-05-01 15:14:15 +0100

    new updated script

--- scripts/system-monitor.sh
+++ scripts/system-monitor.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 ##### Automaion script for system monitoring #####
+ DATE=$(date)
+ echo "$DATE"
+echo "" 
 ##### CPU Usage ######
 echo "CPU Usage Percentage"
-top 
+top -bn1 | grep "Cpu(s)" | awk '{print "CPU Usage Percentage: " 100 - $8 "%"}'
 echo ""
+
 ##### Memory Utilization ######
 echo "Memory Usage"
 free -m
 echo ""
+
 ##### Disk Usgae #######
 echo "Disk Utilization"
 df -h
-
 
